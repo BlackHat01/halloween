0523e7c29b27e4c93e0370bf9b9c9ef1bf54d2c7
Corey Miller <cnmill01@gmail.com>
2017-10-27 11:38:09 -0700

    Intro

--- halloween.py
+++ halloween.py
@@ -3,4 +3,7 @@
 # Matt Gonzalez
 import time
 import sys
-
+time.sleep(2)
+print("""Our tale begins in eighenvalde where the lord of the castle has called for
+heroes to come to his aid and defend him against the mad doctor junkenstein.""")
+      
