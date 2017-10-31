b0b773402ca9116844a8ade893f873618b9144ca
Corey Miller <cnmill01@gmail.com>
2017-10-31 09:54:46 -0700

    More fixes

--- halloween.py
+++ halloween.py
@@ -4,13 +4,13 @@
 import time
 import sys
 time.sleep(1)
-print("""Our tale begins in Eichenvalde where the lord of the castle has called for""")
+print("""Our tale begins in Eichenwalde where the lord of the castle has called for""")
 time.sleep(4)
 print("""heroes to come to his aid and defend him against the mad doctor Junkenstein.""")
 time.sleep(5)
 print("""Our tale is about to begin""")
 time.sleep(3)
-print("""Dr. junkenstien laughed as his minions arose for tonight was the night of""")
+print("""Dr. Junkenstein laughed as his minions arose for tonight was the night of""")
 time.sleep(5)
 print("""Junkenstein's Revenge""")
 time.sleep(3)
