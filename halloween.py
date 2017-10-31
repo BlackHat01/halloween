3ca1949192ca45378b36c2c8bfee7d362dda3e0b
Corey Miller <cnmill01@gmail.com>
2017-10-31 09:52:06 -0700

    Fixes

--- halloween.py
+++ halloween.py
@@ -5,16 +5,13 @@ import time
 import sys
 time.sleep(1)
 print("""Our tale begins in Eichenvalde where the lord of the castle has called for""")
-time.sleep(1)
+time.sleep(4)
 print("""heroes to come to his aid and defend him against the mad doctor Junkenstein.""")
-time.sleep(3)
-print("""(hero specific line)""")
-time.sleep(1)
+time.sleep(5)
 print("""Our tale is about to begin""")
-time.sleep(1)
-print("""Dr. junkenstien laughed as his minions arose for tonight was the night of""")
 time.sleep(3)
+print("""Dr. junkenstien laughed as his minions arose for tonight was the night of""")
+time.sleep(5)
 print("""Junkenstein's Revenge""")
-time.sleep(1)
-print("""Overwatch is a license of Blizzard and we do not own any of their intellectual
-property""")
+time.sleep(3)
+
