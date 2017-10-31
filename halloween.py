d878f54f4062abe3d0918a91371aeedd01a1fd63
Corey Miller <cnmill01@gmail.com>
2017-10-31 10:06:19 -0700

    Improve selection

--- halloween.py
+++ halloween.py
@@ -8,20 +8,33 @@ print("""Our tale begins in Eichenwalde where the lord of the castle has called 
 time.sleep(4)
 print("""heroes to come to his aid and defend him against the mad doctor Junkenstein.""")
 time.sleep(5)
-print("""Our tale is about to begin""")
+print("""Our tale is about to begin...""")
 time.sleep(3)
 print("""Dr. Junkenstein laughed as his minions arose for tonight was the night of""")
 time.sleep(5)
-print("""Junkenstein's Revenge""")
+print("""Junkenstein's Revenge!""")
 time.sleep(3)
 def selectHero():
-    input("""
+    hero = input("""
     Select your hero
 
-        Soldier: 76
+        Soldier 76
         Mccree
         Ana
         Hanzo
 
     (Enter exact name): """)
+    if hero == ("Mccree"):
+        heroName=("Mccree")
+    elif hero == ("Ana"):
+        heroName=("Mccree")
+    elif hero == ("Soldier 76"):
+        heroName=("Mccree")
+    elif hero == ("Hanzo"):
+        heroName=("Mccree")
+    else:
+        print("Invalid selection.")
+        selectHero()
+        
 selectHero()
+ 
